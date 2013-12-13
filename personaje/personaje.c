32cc016521c90d980ba748f6e1278b282829bbe8
elyzabeth <elizabeth.sosa@gmail.com>
2013-12-13 17:27:39 -0300

    Actualizo proyecto personaje con funciones del branch "pruebas"

--- personaje/personaje.c
+++ personaje/personaje.c
@@ -7,10 +7,15 @@
 
 #include "personaje.h"
 
-int test ();
-
 int main (int argc, char *argv[]) {
 
+	// Copiar nombre archivo configuracion
+	if ( argc > 1 ){
+		strncpy(CONFIG_FILE, argv[1], MAXCHARLEN);
+	}
+
+
+	// Registro signal y signal handler
 	signal(SIGINT, per_signal_callback_handler);
 	signal(SIGQUIT, per_signal_callback_handler);
 	signal(SIGUSR1, per_signal_callback_handler);
@@ -18,9 +23,13 @@ int main (int argc, char *argv[]) {
 
 	inicializarPersonaje();
 
-	// TODO agregar logica del personaje
-	//principal(argc, argv);
-	test ();
+	int id_proceso;
+	id_proceso = getpid();
+
+	system("clear");
+	log_info(LOGGER,"************** Iniciando Personaje '%s' (PID: %d) ***************\n", personaje.nombre, id_proceso);
+
+	principal(argc, argv);
 
 	finalizarPersonaje();
 
@@ -30,169 +39,444 @@ int main (int argc, char *argv[]) {
 
 int principal(int argc, char *argv[]) {
 
-	int id_proceso;
+	do {
+		reiniciar(false);
 
-	id_proceso = getpid();
-	system("clear");
+		levantarHilosxNivel();
 
-	log_info(LOGGER,"************** Iniciando Personaje '%s' (PID: %d) ***************\n", personaje.nombre, id_proceso);
+		esperarHilosxNivel();
 
-	levantarHilosxNivel();
+	} while (personaje.reiniciar);
 
-	esperarHilosxNivel();
+	chequearFinTodosLosNiveles ();
 
 	return 0;
 }
 
-void* personajexNivel (t_hilo_personaje *hiloPxN) {
-	int sock = -1;
-	header_t header;
-	fd_set master;
-	fd_set read_fds;
-	int max_desc = 0;
-	int i, ret;
-	int fin = false;
-
-	log_info(LOGGER, "Hilo Personaje para objetivos del nivel %s Iniciado", hiloPxN->personaje.nivel);
-sleep(1);
-
-	/***************** ME CONECTO Y ARMO MENSAJE DE PRESENTACION *******/
-	log_info(LOGGER,"************** CONECTANDOSE  ***************\n");
-	conectar(personaje.ip_orquestador, personaje.puerto_orquestador, &sock);
-
-	FD_ZERO(&master);
-	// Agrego descriptor del Pipe con Nivel.
-	agregar_descriptor(hiloPxN->fdPipe[0], &master, &max_desc);
-
-	agregar_descriptor(sock, &master, &max_desc);
-
-	if (enviarMsjNuevoPersonaje(sock) != EXITO)
-	{
-		log_error(LOGGER,"Error al enviar header NUEVO_PERSONAJE\n\n");
-		fin = true;
-		//return WARNING;
-	}
 
 
-	while(!fin)
-	{
-		FD_ZERO (&read_fds);
-		read_fds = master;
 
-		ret = select(max_desc+1, &read_fds, NULL, NULL, NULL);
+/**
+ * @NAME: inicializarPersonaje
+ * @DESC: Inicializa todas las variables y estructuras necesarias para el proceso personaje
+ */
+void inicializarPersonaje() {
+	// TODO agregar inicializaciones necesarias
 
-		if(ret == -1) {
-			printf("Personaje %s '%c': ERROR en select.", hiloPxN->personaje.nombre, hiloPxN->personaje.id);
-			sleep(1);
-		}
+	levantarArchivoConfiguracionPersonaje(CONFIG_FILE);
 
-		if (ret > 0) {
-			for(i = 0; i <= max_desc; i++)
-			{
+	LOGGER = log_create(configPersonajeLogPath(), "PERSONAJE", configPersonajeLogConsola(), configPersonajeLogNivel() );
+	log_info(LOGGER, "INICIALIZANDO PERSONAJE '%s' ", configPersonajeNombre());
 
-				if (FD_ISSET(i, &read_fds))
-				{
-					// Pregunto si el socket con actividad es el del Pipe
-					if( i == hiloPxN->fdPipe[0])
-					{
-						header_t header;
-						log_info(LOGGER, "Personaje '%c': Recibo mensaje desde Main por Pipe", hiloPxN->personaje.id);
-						read (hiloPxN->fdPipe[0], &header, sizeof(header_t));
+	strcpy(personaje.nombre, configPersonajeNombre());
+	strcpy(personaje.ip_orquestador, configPersonajePlataformaIp());
+	personaje.puerto_orquestador = configPersonajePlataformaPuerto();
 
-						log_debug(LOGGER, "Personaje '%c': mensaje recibido '%d'", hiloPxN->personaje.id, header.tipo);
-						if (header.tipo == FINALIZAR) {
-							log_debug(LOGGER, "Personaje '%c': '%d' ES FINALIZAR", hiloPxN->personaje.id, header.tipo);
-							fin = true;
-							break;
-						}
+	reiniciar(false);
 
-					} else {
-						// Si NO es un mensaje del hilo principal por Pipe es un mensaje del proceso Plataforma.
+	listaHilosxNivel = list_create();
 
-						memset(&header, '\0', sizeof(header_t));
-						recibirHeaderNuevoMsj(sock, &header);
+	pthread_mutex_init (&mutexEnvioMensaje, NULL);
+	pthread_mutex_init (&mutexVidas, NULL);
+	pthread_mutex_init (&mutexListaHilosxNivel, NULL);
+	pthread_mutex_init (&mutexReinicio, NULL);
 
-						// Recino el header con el tipo de mensaje
-						switch (header.tipo)
-						{
-							case PERSONAJE_CONECTADO: log_info(LOGGER,"Personaje Conectado");
-								enviarInfoPersonaje(sock);
-								break;
+	REINTENTOS = 0;
 
-							case OTRO: log_info(LOGGER, "otro");
-								break;
+	inicializarVariablesGlobales();
+}
 
-						}
+void inicializarVariablesGlobales() {
 
+	reiniciar(true);
+	VIDAS = configPersonajeVidas();
 
-					}
-				}
-			}
+	if (planDeNiveles != NULL)
+		queue_destroy_and_destroy_elements(planDeNiveles, (void*)destruirObjetivosxNivel);
 
-		}
-	}
+	planDeNiveles = configPersonajePlanDeNiveles();
 
-	log_info(LOGGER, "FINALIZANDO Hilo Personaje '%c' Nivel %s\n", hiloPxN->personaje.id, hiloPxN->personaje.nivel);
+	list_clean_and_destroy_elements(listaHilosxNivel, (void*)destruirEstructuraHiloPersonaje);
+}
 
-	pthread_exit(NULL);
+void reiniciar(bool valor) {
+	pthread_mutex_lock (&mutexReinicio);
+	personaje.reiniciar = valor;
+	pthread_mutex_unlock (&mutexReinicio);
+}
+
+/**
+ * @NAME: finalizarPersonaje
+ * @DESC: Finaliza todas las variables y estructuras que fueron creadas para el proceso personaje
+ */
+void finalizarPersonaje() {
+	log_info(LOGGER, "FINALIZANDO PROCESO PERSONAJE\n");
+	reiniciar(false);
+
+	// TODO Bajar Hilos
+	finalizarHilosPersonaje();
+
+	pthread_mutex_destroy(&mutexEnvioMensaje);
+	pthread_mutex_destroy(&mutexVidas);
+	pthread_mutex_destroy(&mutexListaHilosxNivel);
+	pthread_mutex_destroy(&mutexReinicio);
+
+	list_destroy_and_destroy_elements(listaHilosxNivel, (void*)destruirEstructuraHiloPersonaje);
+	queue_destroy_and_destroy_elements(planDeNiveles, (void*)destruirObjetivosxNivel);
+
+	destruirConfigPersonaje();
+	log_destroy(LOGGER);
 }
 
 
-int test () {
+/**
+ * @NAME: finalizarHilosPersonaje
+ * @DESC: Finaliza los hilos de personajes creados para cada nivel enviandoles por pipe el mensaje FINALIZAR
+ */
+void finalizarHilosPersonaje() {
+	int i = 0;
+	int32_t cantHilosPersonaje;
 
-	//personaje_t personaje; // Lo hago global para poder inicializarlo desde funcion
-	int id_proceso;
-	int sock = -1;
+	cantHilosPersonaje = list_size(listaHilosxNivel);
+
+	if(cantHilosPersonaje <= 0)
+		return;
+
+	log_info(LOGGER, "FINALIZANDO HILOS Personaje");
+
+	void _finalizar_hilo(t_hilo_personaje *hPersonaje) {
+
+		log_debug(LOGGER, "\n\nHILO %d/de %d) Envio mensaje de FINALIZAR a hilo Personaje '%c' (%s) - por Pipe socket: %d", ++i, cantHilosPersonaje, hPersonaje->personaje.id, hPersonaje->personaje.nivel, hPersonaje->fdPipe[1]);
+
+		// write(hPersonaje->fdPipe[1], buffer_header, sizeof(header_t));
+		enviarMsjPorPipePJ(hPersonaje->fdPipe[1], FINALIZAR);
+		pthread_join(hPersonaje->tid, NULL);
+		//sleep(1);
+	}
+
+	list_iterate(listaHilosxNivel, (void*)_finalizar_hilo);
+
+}
+
+void esperarHilosxNivel() {
+
+
+	void _join_thread (t_hilo_personaje *hilo){
+		pthread_join(hilo->tid, NULL);
+	}
+
+	list_iterate(listaHilosxNivel, (void*)_join_thread);
+
+}
+
+
+void crearHiloxNivel(t_objetivosxNivel *oxn, t_hilo_personaje *hiloPersonaje) {
+
+	hiloPersonaje = crearEstructuraHiloPersonaje(oxn);
+
+	list_add(listaHilosxNivel, hiloPersonaje);
+
+	log_debug(LOGGER, "Hilo para nivel %s", oxn->nivel);
+	log_debug(LOGGER, "%s de %s pipe: %d y %d", hiloPersonaje->personaje.nombre, hiloPersonaje->personaje.nivel, hiloPersonaje->fdPipe[0], hiloPersonaje->fdPipe[1]);
+
+	// Creo el hilo para el nivel
+	pthread_create (&(hiloPersonaje->tid), NULL, (void*)personajexNivel, (t_hilo_personaje*)hiloPersonaje);
+
+}
+
+
+void levantarHilosxNivel() {
+	int i;
+	int cant = queue_size(planDeNiveles);
+	t_objetivosxNivel *oxn = NULL;
+	t_hilo_personaje *hiloPersonaje = NULL;
+
+	for (i = 0; i < cant; i++) {
+
+		oxn = queue_pop(planDeNiveles);
+		queue_push(planDeNiveles, oxn);
+
+		crearHiloxNivel( oxn, hiloPersonaje );
+
+	}
+
+}
+
+
+void levantarHiloxNivel(char *nivel) {
+	int i;
+	int cant = queue_size(planDeNiveles);
+	t_objetivosxNivel *oxn = NULL;
+	t_hilo_personaje *hiloPersonaje = NULL;
+
+	for (i = 0; i < cant; i++) {
+
+		oxn = queue_pop(planDeNiveles);
+		queue_push(planDeNiveles, oxn);
+
+		if ( strcasecmp(oxn->nivel, nivel) == 0 )
+			crearHiloxNivel ( oxn, hiloPersonaje );
+
+	}
+
+}
+
+
+t_hilo_personaje* crearEstructuraHiloPersonaje(t_objetivosxNivel *oxn) {
+	t_hilo_personaje *hiloPersonaje;
+
+	hiloPersonaje = calloc(1, sizeof(t_hilo_personaje));
+
+	strcpy(hiloPersonaje->personaje.nombre, configPersonajeNombre());
+	strcpy(hiloPersonaje->personaje.nivel, oxn->nivel);
+	hiloPersonaje->objetivos = *oxn;
+	hiloPersonaje->personaje.id = configPersonajeSimbolo();
+	hiloPersonaje->personaje.recurso = '-';
+	hiloPersonaje->personaje.posActual.x = 0;
+	hiloPersonaje->personaje.posActual.y = 0;
+	hiloPersonaje->moverPorX=true;
+
+	if (pipe(hiloPersonaje->fdPipe) == -1)
+	{
+		perror ("crearEstructuraHiloPersonaje: No se puede crear Tuberia de comunicacion.");
+		exit (-1);
+	}
+log_debug(LOGGER, "%s pipe: %d y %d", hiloPersonaje->personaje.nombre, hiloPersonaje->fdPipe[0], hiloPersonaje->fdPipe[1]);
+
+	return hiloPersonaje;
+}
+
+void destruirEstructuraHiloPersonaje(t_hilo_personaje* hiloPersonaje) {
+
+	close(hiloPersonaje->fdPipe[0]);
+	close(hiloPersonaje->fdPipe[1]);
+
+	free(hiloPersonaje);
+}
+
+
+int chequearFinTodosLosNiveles () {
+	// TODO	Al concluir todos los niveles, se conectará al Orquestador y notificará que concluyó su
+	// plan de niveles. Este lo moverá a una cola de finalizados y lo dejará a la espera de que
+	// los demás Personajes terminen.
+
+	int finPlanNiveles = 1;
+
+	int _checkEnd(t_hilo_personaje *hilo) {
+		log_debug(LOGGER, "chequearFinTodosLosNiveles: %s - hilo->estado == %s", hilo->personaje.nivel, getStringTipo(hilo->estado));
+		finPlanNiveles = finPlanNiveles && (hilo->estado == PLAN_NIVEL_FINALIZADO);
+		return finPlanNiveles;
+	}
+	list_iterate(listaHilosxNivel, (void*)_checkEnd);
+
+	if (finPlanNiveles) {
+
+		log_info(LOGGER, "\n\nFINALICE TODO EL PLAN DE NIVELES!!!!!\n\n");
+		imprimirVidasyReintentos();
+
+		// informar al orquestador.
+		enviarMsjPlanDeNivelesConcluido();
+
+	} else {
+		log_info(LOGGER, "\n\nSaliendo del Proceso Personaje sin finalizar todo el plan de niveles.!!!!!\n\n");
+	}
+
+	return 0;
+}
+
+
+int enviarMsjPorPipePJ (int32_t fdPipe, char msj) {
+	int ret;
+	header_t header;
+	char* buffer_header = calloc(1,sizeof(header_t));
+
+	initHeader(&header);
+	header.tipo = msj;
+	header.largo_mensaje=0;
+
+	memset(buffer_header, '\0', sizeof(header_t));
+	memcpy(buffer_header, &header, sizeof(header_t));
+
+	ret =  write(fdPipe, buffer_header, sizeof(header_t));
+
+	free(buffer_header);
+
+	return ret;
+}
+
+void enviarMsjPlanDeNivelesConcluido() {
+	int ret, sock = -1;
 	header_t header;
 
-	char *buffer_header;
-	buffer_header = calloc(1,sizeof(header_t));
-
-	//char *buffer_header; // Lo hago global para poder inicializarlo y liberarlo desde funcion
-	/************************************ Inicio **************************************/
-	/*
-	  if(argc != 3)
-	  {
-	    	puts("Error cantidad Parametros");
-	  }
-	 */
-	id_proceso = getpid();
-	system("clear");
-
-	log_info(LOGGER,"************** Iniciando Personaje '%s' (PID: %d) ***************\n", personaje.nombre, id_proceso);
-	//cambiar_nombre_proceso(argv,argc, personaje.nombre);
-
-	/***************** ME CONECTO Y ARMO MENSAJE DE PRESENTACION *******/
-	log_info(LOGGER,"************** CONECTANDOSE  ***************\n");
 	conectar(personaje.ip_orquestador, personaje.puerto_orquestador, &sock);
 
+	initHeader(&header);
+	header.tipo = PLAN_NIVELES_CONCLUIDO;
+	header.largo_mensaje = 0;
+	header.id[0] = configPersonajeSimbolo();
 
-	if (enviarMsjNuevoPersonaje(sock) != EXITO)
-	{
-		log_error(LOGGER,"Error al enviar header NUEVO_PERSONAJE\n\n");
-		return WARNING;
+	log_info(LOGGER, "\n\nEnviando PLAN_NIVELES_CONCLUIDO al orquestador.");
+	if ((ret = enviar_header(sock, &header)) != EXITO) {
+		log_error(LOGGER, "\n\nERROR AL ENVIAR PLAN_NIVELES_CONCLUIDO al ORQUESTADOR!!!\n\n");
+	}
+}
+
+// TODO el mensaje de muerte lo envia el hilo pricipal del personaje al orquestador?
+// o el hilo principal se lo comunica a cada hilo hijo y estos se lo comunican a sus respectivos planificadores??
+void enviarMsjMuerteDePersonajeAlOrq () {
+	int ret, sock = -1;
+	header_t header;
+
+	conectar(personaje.ip_orquestador, personaje.puerto_orquestador, &sock);
+
+	initHeader(&header);
+	header.tipo = MUERTE_PERSONAJE;
+	header.largo_mensaje = 0;
+	header.id[0] = configPersonajeSimbolo();
+
+	log_info(LOGGER, "\n\nEnviando MUERTE_PERSONAJE al orquestador.");
+	if ((ret = enviar_header(sock, &header)) != EXITO) {
+		log_error(LOGGER, "\n\nERROR AL ENVIAR MUERTE_PERSONAJE al ORQUESTADOR!!!\n\n");
 	}
 
-	while(1)
-	{
+}
 
-		recibir (sock, buffer_header, sizeof(header_t));
-		memcpy(&header, buffer_header, sizeof(header_t));
 
-		switch (header.tipo) /*recibo estado */
-		{
-			case PERSONAJE_CONECTADO: log_info(LOGGER,"Personaje Conectado");
-				enviarInfoPersonaje(sock);
-			break;
+void imprimirVidasyReintentos() {
+	log_info(LOGGER, "\n\nVIDAS Y REINTENTOS de %s\n******************\nVIDAS: %d\nREINTENTOS: %d\n\n", personaje.nombre, VIDAS, REINTENTOS);
+}
 
-			case OTRO: log_info(LOGGER, "otro");
-			break;
 
-		}
+int32_t incrementarVida() {
+	pthread_mutex_lock (&mutexVidas);
+	VIDAS++;
+	log_info(LOGGER, "\n\nEl Personaje incrementa vidas.\n");
+	imprimirVidasyReintentos();
+	//TODO agregar logica luego de incrementar vidas si corresponde
+	pthread_mutex_unlock (&mutexVidas);
 
-		sleep(15);
+	return VIDAS;
+}
+
+int32_t decrementarVida() {
+
+	pthread_mutex_lock (&mutexVidas);
+	int ret;
+	if(VIDAS > 0) {
+		VIDAS--;
+		log_info(LOGGER, "Personaje decrementa 1 vida.\n");
+		imprimirVidasyReintentos();
+		ret = VIDAS;
+	} else {
+		log_info(LOGGER, "\nPersonaje No tiene VIDAS disponibles.\n");
+		imprimirVidasyReintentos();
+		ret = -1;
+	}
+	pthread_mutex_unlock (&mutexVidas);
+
+	return ret;
+}
+
+
+
+
+
+/*
+ * Si no le quedaran vidas disponibles, el Personaje deberá interrumpir todos sus planes de
+ * niveles y mostrar en pantalla un mensaje preguntando al usuario si desea reiniciar el juego,
+ * informando también la cantidad de reintentos que ya se realizaron. De aceptar, el Personaje
+ * incrementará su contador de reintentos y reiniciará su Plan de Niveles. En caso negativo, el
+ * Personaje se cerrará, abandonando el juego.
+ */
+void manejoSIGTERM() {
+
+	int vidas_restantes = decrementarVida();
+
+	if (vidas_restantes == -1) {
+		reinicioNivelCompleto();
 	}
 
-	//free (buffer_header); /* Solo al final porque lo uso siempre */ Movido a finalizarPersonaje()
-	return 0;
+}
+
+void reinicioNivelCompleto() {
+
+	char respuesta;
+	// interrumpir todos los planes de niveles
+	// llamar funcion que baje los hilos y ver que otras variables hay que reiniciar!!
+	finalizarHilosPersonaje();
+	sleep(2);
+
+	printf("\n\n\n Knock, knock, Neo...\n\n");
+	sleep(2);
+	printf("You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe....\n\n");
+	sleep(2);
+	printf("You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.\n...\n");
+	sleep(2);
+	printf("Remember that all I am offering is the truth. Nothing more. \n");
+	printf("\nThis is your last chance. After this there is no turning back... \n\n Choose blue or red pill b/r: ");
+	printf("\n(traducción: Desea reiniciar el juego? s/n:) ");
+
+	while ((respuesta=getc(stdin)) != 's' && respuesta != 'b' && respuesta != 'n' && respuesta != 'r')
+		printf("\nPor favor ingrese 's' o 'r' para reiniciar, o ingrese 'n' o 'b' para terminar: ");
+
+	if(respuesta == 's' || respuesta == 'r') {
+		REINTENTOS++;
+		log_info(LOGGER, "\n\nVOLVIENDO A MATRIX...\n\nREINICIANDO EL JUEGO...");
+
+		// llamar funcion que reinicie el juego
+		inicializarVariablesGlobales();
+
+		imprimirVidasyReintentos();
+		sleep(2);
+
+
+	} else {
+		log_info(LOGGER, "\n\nSALIENDO DE MATRIX....\n\n CERRANDO PROCESO PERSONAJE");
+
+		finalizarPersonaje();
+
+		exit(0);
+	}
+}
+
+/*
+ * @NAME: per_signal_callback_handler
+ * @DESC: Define la funcion a llamar cuando una señal es enviada al proceso
+ * ctrl-c (SIGINT)
+ */
+void per_signal_callback_handler(int signum)
+{
+	log_info(LOGGER, "INTERRUPCION POR SEÑAL: %d = %s \n", signum, strsignal(signum));
+
+	switch(signum) {
+
+	case SIGUSR1: // SIGUSR1=10 ( kill -s USR1 <PID> )
+		log_info(LOGGER, " - LLEGO SEÑAL SIGUSR1\n");
+		//Debo incrementar 1 vida
+		incrementarVida();
+		break;
+	case SIGTERM: // SIGTERM=15 ( kill <PID>)
+		log_info(LOGGER, " - LLEGO SEÑAL SIGTERM\n");
+		manejoSIGTERM();
+		break;
+	case SIGINT: // SIGINT=2 (ctrl-c)
+		log_info(LOGGER, " - LLEGO SEÑAL SIGINT\n");
+		finalizarPersonaje();
+		// Termino el programa
+		exit(signum);
+		break;
+	case SIGKILL: // SIGKILL=9 ( kill -9 <PID>)
+		log_info(LOGGER, " - LLEGO SEÑAL SIGKILL\n");
+		finalizarPersonaje();
+		// Termino el programa
+		exit(signum);
+		break;
+	case SIGQUIT: // SIGQUIT=3 (ctrl-4 o kill -s QUIT <PID>)
+		log_info(LOGGER, " - LLEGO SEÑAL SIGQUIT\n");
+		finalizarPersonaje();
+		// Termino el programa
+		exit(signum);
+		break;
+	}
+
 }
