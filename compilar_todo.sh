c783db098bd2f462928a0fcc1403518a54faa077
elyzabeth <elizabeth.sosa@gmail.com>
2013-12-13 17:35:19 -0300

    Agrego scripts necesarios para Deploy & Setup

--- compilar_todo.sh
+++ compilar_todo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=`pwd`
 
-for x in so-commons-library plataforma personaje nivel-gui nivel 
+for x in so-commons-library plataforma personaje nivel-gui nivel fileSystem
 do
 	cd ${x}/Debug/
 	make clean
